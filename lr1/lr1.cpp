7b736f2213ce12b0a4b4e662ad6cee20d44a20d6
AshWays <105866621+AshWays@users.noreply.github.com>
2023-06-14 12:48:00 +0300

    Add files via upload

---
+++ lr1/lr1.cpp
@@ -0,0 +1,101 @@
+﻿#include <iostream>
+#define N 7
+
+using namespace std;
+
+bool sum(int pos);
+void rework(int pos);
+bool in(int pos);
+void reworking(int yus);
+bool isPreem(int pos);
+
+struct maxX {
+	int figure, yarus;
+};
+						  /*x1 x2 x3 x4 x5 x6 x7*/
+int masX[N][N] = { /*x1*/ { 0, 1, 1, 0, 0, 0, 0 },
+				   /*x2*/ { 0, 0, 1, 0, 0, 0, 0 },
+				   /*x3*/ { 0, 0, 0, 0, 0, 1, 1 },
+				   /*x4*/ { 0, 1, 0, 0, 0, 1, 0 },
+			       /*x5*/ { 0, 1, 0, 1, 0, 1, 0 },
+			       /*x6*/ { 0, 0, 0, 0, 0, 0, 1 },
+				   /*x7*/ { 0, 0, 0, 0, 0, 0, 0 } };
+
+
+						  /*x1 x2 x3 x4 x5*/
+//int masX[N][N] = { /*x1*/ { 0, 1, 1, 0, 0 },
+	//			   /*x2*/ { 0, 0, 1, 1, 0 },
+		//		   /*x3*/ { 0, 0, 0, 1, 1 },
+			//	   /*x4*/ { 0, 0, 0, 0, 0 },
+				//   /*x5*/ { 0, 0, 0, 0, 0 } };
+
+maxX masMax[N] = { 0 };
+
+int main()
+{                           
+	int yars = 1,clock = 0;
+	maxX x;
+
+	while (clock < N) {
+		for (int i = 0; i < N; i++)
+			if (!in(i + 1)) {
+				if (sum(i) and isPreem(i)) {
+					x.figure = i + 1;
+					x.yarus = yars;
+					masMax[clock] = x;
+					clock++;
+					cout << "String x" << i + 1 << " addet to maxX, yarus: " << yars << "\n";
+				}
+				else if(sum(i)){
+					x.figure = i + 1;
+					x.yarus = yars+1;
+					masMax[clock] = x;
+					clock++;
+					cout << "String x" << i + 1 << " addet to maxX, yarus: " << "best" << "\n";
+				}
+			}
+		reworking(yars);
+		yars++;
+	}
+
+
+	return 0;
+}
+
+bool sum(int pos) {
+	int numb = 0;
+	for (int i = 0; i < N; i++) {
+		numb += masX[pos][i];
+	}
+	if (numb) return false;
+	else return true;
+}
+
+bool in(int pos) {
+	for (int i = 0; i < N; i++) {
+		if (pos == masMax[i].figure) return true;
+	}
+	return false;
+}
+
+void rework(int pos) {
+	for (int i = 0; i < N; i++) {
+		masX[i][pos] = 0;
+	}
+}
+
+void reworking(int yus) {
+	for (int i = 0; i < N; i++) {
+		if (yus==masMax[i].yarus)
+			rework((masMax[i]).figure-1);
+	}
+}
+
+bool isPreem(int pos) {
+	int numb = 0;
+	for (int i = 0; i < N; i++) {
+		numb += masX[i][pos];
+	}
+	if (numb) return true;
+	else return false;
+}
---
+++ lr2/lr2.cpp
@@ -0,0 +1,184 @@
+﻿#include <iostream>
+#define N 7
+
+using namespace std;
+
+void calculateXp(int ** Xp);
+void calculateXm(int** Xm);
+void printXmp(int** Xmp,bool pm);
+void calculateU(float* U,int ** Xp,int ** Xm);
+bool concatinate(int* p, int* m);
+
+                           /*x1 x2 x3 x4 x5 x6 x7 */
+int X[N][N] = {     /*x1*/ { 0, 0, 1, 0, 0, 0, 0  },
+                    /*x2*/ { 1, 0, 0, 0, 1, 0, 0  },
+                    /*x3*/ { 0, 0, 0, 1, 0, 0, 0  },
+                    /*x4*/ { 0, 0, 1, 0, 0, 1, 0  },
+                    /*x5*/ { 1, 0, 1, 0, 0, 0, 1  },
+                    /*x6*/ { 0, 0, 0, 0, 1, 0, 0  },
+                    /*x7*/ { 0, 0, 1, 0, 1, 0, 0  } };
+
+int** Xp = new int*[N-1];
+int** Xm = new int* [N-1];
+float* U = new float[N];
+
+int main()
+{
+    for (int i = 0; i < N - 1; i++) {
+        Xp[i] = NULL;
+        Xm[i] = NULL;
+    }
+    for (int i = 0; i < N; i++) {
+        U[i] = 0;
+    }
+
+    calculateXp(Xp);
+    calculateXm(Xm);
+    calculateU(U,Xp,Xm);
+
+    printXmp(Xp,true);
+    cout << "\n";
+    printXmp(Xm,false);
+    cout << "\n";
+    for (int i = 0; i < N; i++) {
+        cout << "U[" << i+1 << "] = " << U[i] << "\n";
+    }
+    cout << "\n";
+    
+}
+
+void calculateXp(int** Xp) {
+    bool exist = false;
+    for (int i = 1; i < N; i++) {
+        exist = false;
+        for (int j = 0; j < i; j++) {
+            if (X[j][i] == 1) {
+                if (!exist) {
+                    Xp[i - 1] = new int [N];
+                    exist = true;
+                    for (int k = 0; k < N; k++) {
+                        Xp[i - 1][k] = 0;
+                    }
+                }
+                Xp[i - 1][j] = 1;
+            }
+        }
+    }
+}
+
+void calculateXm(int** Xm) {
+    bool exist = false;
+    for (int i = 1; i < N; i++) {
+        exist = false;
+        for (int j = 0; j < i; j++) {
+            if (X[i][j] == 1) {
+                if (!exist) {
+                    Xm[i - 1] = new int[N];
+                    exist = true;
+                    for (int k = 0; k < N; k++) {
+                        Xm[i - 1][k] = 0;
+                    }
+                }
+                Xm[i - 1][j] = 1;
+            }
+        }
+    }
+}
+
+void printXmp(int** Xmp,bool pm) {
+    for (int i = 0; i < N - 1; i++) {
+        if (pm) cout << "X+";
+        else cout << "X-";
+        cout << i + 1 << ": ";
+        if (Xmp[i] != NULL) {
+            for (int j = 0; j < N; j++) {
+                if (Xmp[i][j] == 1) {
+                    cout << "x" << j + 1 << ", ";
+                }
+            }
+        }
+        else cout << "Null";
+        cout << "\n";
+    }
+}
+
+void calculateU(float* U, int** Xp, int** Xm){
+    int* conc; double max,min;
+    for (int i = 0; i < N - 1; i++) {
+        if ((Xp[i] == NULL)&&(Xm[i] != NULL)) {
+            for (int j = 0; j < i+1; j++) {
+                if (Xm[i][j] == 1) {
+                    min = U[j];
+                    break;
+                }
+            }
+            for (int j = 0; j < i + 1; j++) {
+                if ((Xm[i][j] == 1)&&(U[j]<min)) {
+                    min = U[j];
+                }
+            }
+            U[i + 1] = min + 1;
+        }
+        else if ((Xp[i] != NULL) && (Xm[i] == NULL)) {
+            for (int j = 0; j < i + 1; j++) {
+                if (Xp[i][j] == 1) {
+                    max = U[j];
+                    break;
+                }
+            }
+            for (int j = 0; j < i + 1; j++) {
+                if ((Xp[i][j] == 1) && (U[j] > max)) {
+                    max = U[j];
+                }
+            }
+            U[i + 1] = max - 1;
+        }
+        else {
+            if (concatinate(Xp[i], Xm[i])) {
+                for (int j = 0; j < i + 1; j++) {
+                    if ((Xm[i][j] == 1)&& (Xp[i][j] == 1)) {
+                        U[i+1] = U[j];
+                        break;
+                    }
+                }
+            }
+            else {
+                for (int j = 0; j < i + 1; j++) {
+                    if (Xm[i][j] == 1) {
+                        min = U[j];
+                        break;
+                    }
+                }
+                for (int j = 0; j < i + 1; j++) {
+                    if ((Xm[i][j] == 1) && (U[j] < min)) {
+                        min = U[j];
+                    }
+                }
+                for (int j = 0; j < i + 1; j++) {
+                    if (Xp[i][j] == 1) {
+                        max = U[j];
+                        break;
+                    }
+                }
+                for (int j = 0; j < i + 1; j++) {
+                    if ((Xp[i][j] == 1) && (U[j] > max)) {
+                        max = U[j];
+                    }
+                }
+                U[i + 1] = (min + max) / 2.0;
+            }
+
+
+            
+        }
+    }
+
+}
+
+bool concatinate(int* p, int* m) {
+    for (int i = 0; i < N; i++) {
+        if ((p[i] == 1) && (m[i] == 1))
+            return true;
+    }
+    return false;
+}
